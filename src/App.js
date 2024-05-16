beae79eca8868aa3f5816bcb740dc670f7f938ce
RahulNaidu2104 <rahulnaidubp@gmail.com>
2024-05-16 09:11:31 +0530

    Conditional Rendering Functional Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 //import Welcome from './index_FirstExample_FunctionalComponent_ClassComponent.js';
-import StringCount from './StringCount.js';
+//import StringCount from './StringCount.js';
+import StudentLogIn from './Components/ConditionalFunctionalComponent.js';
 
 function App() {
   return (
     <div className="App">
-        <StringCount name="abbcccdddd"/>
+        {/* <StringCount name="abbcccdddd"/> */}
+        <StudentLogIn name="Rahul" sportsname="Cricket" />
     </div>
   );
 }
